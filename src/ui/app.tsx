1ad9e9bde3b2c22565460c8413a38ec0a92f5ffe
Maksim Karabutov <max.karabutoff2010@yandex.ru>
2021-03-11 21:21:48 +0400

    Introduced eslint (#2)

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -3,9 +3,9 @@ import OverviewPage from './pages/overview';
 import './app.scss';
 
 const App: FunctionComponent = function () {
-	return (<div class="app">
-		<OverviewPage />
-	</div>);
+  return (<div class="app">
+    <OverviewPage />
+  </div>);
 };
 
 export default App;
